df9fa9e1df919ded7307671744169d37c52f3867
itsZling <micahgo425@gmail.com>
2026-03-31 16:01:34 -0400

    Fixed imports

--- frontend/expense-tracker/src/utils/axiosInstance.js
+++ frontend/expense-tracker/src/utils/axiosInstance.js
@@ -1,9 +1,9 @@
-import axoios from "axios";
+import axios from "axios";
 import { BASE_URL } from "./apiPaths";
 
-const axiosInstance = axoios.create({
+const axiosInstance = axios.create({
     baseURL: BASE_URL,
-    timeout: 10000,
+    timeout: 60000,
     headers: {
         "Content-Type": "application/json",
         Accept: "application/json",
